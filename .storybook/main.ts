f8b4c0bdf2dcfc5f0e895f72db28b318c3a64088
Марченко Владислав Валерьевич <w3k5@yandex.ru>
2024-07-04 09:44:03 +0300

    feat(custom): документация по работе с иконками

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,11 +1,12 @@
 import type { StorybookConfig } from "@storybook/react-vite";
 
 const config: StorybookConfig = {
-  stories: ["../src/**/*.stories.tsx"],
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.tsx"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
+    "@storybook/addon-docs",
   ],
   framework: {
     name: "@storybook/react-vite",
